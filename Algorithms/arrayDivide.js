7041d61544dfedcb9f7a3bba1edc57128299ab6b
Daniel <daniel.lam@8x8.com>
2019-11-12 22:25:19 -0800

    Updated new algorithms

--- Algorithms/arrayDivide.js
+++ Algorithms/arrayDivide.js
@@ -5,29 +5,61 @@
 // Input: [1,2,3,4,5,6,7,8],3
 // Output: [[1,2,3],[4,5,6],7,8]
 
+// function arrayDivide(array,int){
+//     const result = [];
+//     let newArray = [];
+//     let position = array.length;
+//     // console.log(position)
+//     let count = 0;
+//     const stop = array.length%int;
+//     // console.log(stop)
+//     for( let i = 0; i<array.length;i++){
+//         if(position === stop){
+//             break
+//         }
+//         if(count<int){
+//             newArray.push(array[i])
+//             count++;
+//         }
+//         if(count === int){
+//             result.push(newArray);
+//             newArray = [];
+//             count = 0;
+//         }
+//         position --;
+//     }
+//     for(let i = 0;i<stop;i++){
+//         result.push(array[array.length-stop+i])
+//     }
+//     return result;
+// }
+
+// console.log(arrayDivide([1,2,3,4,5,6,7,8],3));
+
 function arrayDivide(array,int){
     const result = [];
     let newArray = [];
-    let position = array.length;
+    // console.log(position)
     let count = 0;
-    const stop = array.length%int;
+    let remainder = array.length%int;
+    const stop = array.length - remainder;
+    // console.log(stop)
     for( let i = 0; i<array.length;i++){
-        if(position === stop){
+        if(i === stop){
             break
         }
         if(count<int){
             newArray.push(array[i])
             count++;
         }
-        if(count === int-1){
+        if(count === int){
             result.push(newArray);
             newArray = [];
             count = 0;
         }
-        position --;
     }
-    for(let i = 0;i<stop;i++){
-        result.push(array[array.length-stop+i])
+    for(let i = 0;i<remainder;i++){
+        result.push(array[stop+i])
     }
     return result;
 }
