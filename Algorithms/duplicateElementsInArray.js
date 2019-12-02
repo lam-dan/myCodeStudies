aef6286928fab85cb7c6025228ecdc85b76b3dd9
Daniel <danlam@ucdavis.edu>
2019-12-02 12:47:28 -0800

    Updated duplicateElementsInArray

--- Algorithms/duplicateElementsInArray.js
+++ Algorithms/duplicateElementsInArray.js
@@ -1,16 +1,19 @@
+function duplicateElements(arr) {
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    arr.push(0);
+  }
 
-function duplicateElements(arr){
-    let length = arr.length;
-    for(let i = 0; i< length;i++){
-        arr.push(0);
-    }
-    return arr;
-    let j = Math.floor(arr.length/2);
-    let k = arr.length-1;
-    while(j >= 0){
-        arr[j]
-    }
+  let stop = Math.floor(arr.length / 2);
+  let k = arr.length - 1;
+  let j = Math.floor(arr.length / 2) - 1;
 
+  while (k >= stop) {
+    arr[k] = arr[j];
+    j--;
+    k--;
+  }
+  return arr;
 }
 
-console.log(duplicateElements([1,2,3]));
+console.log(duplicateElements([1, 2, 3]));
