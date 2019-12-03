808a88eff5c84f84393b88bd260767704fec05f4
Daniel <danlam@ucdavis.edu>
2019-12-03 01:10:07 -0800

    Updated reverseNumber algorithm

--- Algorithms/mergeSort.js
+++ Algorithms/mergeSort.js
@@ -1,31 +1,45 @@
-function merge(left, right) {
-    let arr = [];
-    while (left.length && right.length) {
-      if (left[0] < right[0]) {
-        arr.push(left.shift());
-      } else {
-        arr.push(right.shift());
-      }
-    }
-    return arr.concat(left.slice().concat(right.slice()));
-  }
-  
-  function mergeSort(arr) {
-    // Fast exit to check if there is only one value in the array
-    if (arr.length < 2) {
-      return arr;
-    }
-    // Calculate middle index of array
-    const middle = Math.floor(arr.length / 2);
-    // Create left side of the array
-    const left = arr.slice(0, middle);
-    // Create right side of the array
-    const right = arr.slice(middle);
-    // Recursively repeat process for left side of the array & right side of the array
-    // until we have individual elements of the array
-    // Each time we are recurseively repeating the division process, we call the merge method on each divison
-    // https://www.youtube.com/watch?v=4VqmGXwpLqc
-    return merge(mergeSort(left), mergeSort(right));
-  }
+// Time Complexity of O(n log n)
 
-  console.log(mergeSort([5,7,9,2,3,1]))
+function merge(left, right) {
+  let arr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+  return arr.concat(left.slice().concat(right.slice()));
+}
+
+function mergeSort(arr) {
+  // Fast exit to check if there is only one value in the array
+  if (arr.length < 2) {
+    return arr;
+  }
+  // Calculate middle index of array
+  const middle = Math.floor(arr.length / 2);
+  // Create left side of the array
+  const left = arr.slice(0, middle);
+  // Create right side of the array
+  const right = arr.slice(middle);
+  // Recursively repeat process for left side of the array & right side of the array
+  // until we have individual elements of the array
+  // Each time we are recurseively repeating the division process, we call the merge method on each divison
+  // https://www.youtube.com/watch?v=4VqmGXwpLqc
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+console.log(mergeSort([5, 7, 9, 2, 3, 1]));
+
+
+
+
+
+
+function merge(left,right)
+
+function mergeSort(array){
+
+
+}
--- Algorithms/reverseNumber.js
+++ Algorithms/reverseNumber.js
@@ -1,16 +1,21 @@
-// Write a function to reverse a number
-
+// Write a function to reverse any number
 function get_reverse_of_int(int) {
   let remainder = 0;
   let lastNumber;
-  let intLength = int % 10;
+  let negative;
+  if (int < 0) {
+    int = int * -1;
+    negative = true;
+  }
+
+  let intLength = Math.ceil(Math.log10(int + 1));
+
   for (let i = intLength; i > 0; i--) {
     lastNumber = int % 10;
-    remainder += lastNumber * Math.pow(10, lastNumber - 1);
+    remainder += lastNumber * Math.pow(10, i - 1);
     int = Math.floor(int / 10);
   }
-  return remainder;
+  return negative ? remainder * -1 : remainder;
 }
 
-console.log(get_reverse_of_int(1234567));
-
+console.log(get_reverse_of_int(-678));
