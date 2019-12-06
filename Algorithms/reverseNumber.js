2bf1eb423405f978c742fe61666a192e455e7568
Daniel <oculus@Daniels-MacBook-Pro.local>
2019-12-05 21:12:56 -0800

    Updated reverseNumber algorithm

--- Algorithms/reverseNumber.js
+++ Algorithms/reverseNumber.js
@@ -1,21 +1,21 @@
 // Write a function to reverse any number
 function get_reverse_of_int(int) {
-  let remainder = 0;
-  let lastNumber;
-  let negative;
-  if (int < 0) {
-    int = int * -1;
-    negative = true;
-  }
+	let remainder = 0
+	let lastNumber
+	let negative
 
-  let intLength = Math.ceil(Math.log10(int + 1));
+	if (int < 0) {
+		int = int * -1
+		negative = true
+	}
 
-  for (let i = intLength; i > 0; i--) {
-    lastNumber = int % 10;
-    remainder += lastNumber * Math.pow(10, i - 1);
-    int = Math.floor(int / 10);
-  }
-  return negative ? remainder * -1 : remainder;
+	while (int) {
+		lastNumber = int % 10
+		int = Math.floor(int / 10)
+		remainder = remainder * 10 + lastNumber
+	}
+
+	return negative ? remainder * -1 : remainder
 }
 
-console.log(get_reverse_of_int(123456789));
+console.log(get_reverse_of_int(123456789))
